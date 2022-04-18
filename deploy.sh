d16ecb6e1085bbd67eef98da8839dfde58180657
bde_100008627096_erica <bde_100008627096_erica@git.cloud.tencent.com>
2022-04-18 19:51:59 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 git rm -r --cached . 
-git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin git@github.com:ericasun/vscode.git &&
 git push -f -u origin main &&
 cd -
 echo https://ericasun.github.io/jrg-Vue3-wheel-pratice-11-03/index.html
